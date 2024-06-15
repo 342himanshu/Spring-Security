9e5a9694a984a4bd016749228e5584b22805459b
342himanshu <342himanshu@gmail.com>
2024-06-15 15:36:00 +0530

    adding cors and csrf

--- src/main/java/com/eazybytes/repository/CustomerRepository.java
+++ src/main/java/com/eazybytes/repository/CustomerRepository.java
@@ -1,12 +1,14 @@
 package com.eazybytes.repository;
 
 import com.eazybytes.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
     List<Customer> findByEmail(String email);
+    
 }
